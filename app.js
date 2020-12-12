77b3a979a2ffd1b3ddc673904db680107836121f
Victor Nastasa <victornast@gmail.com>
2020-12-12 17:18:34 +0100

    change to handlebars

--- app.js
+++ app.js
@@ -4,21 +4,30 @@ const app = express();
 // Making files "public"
 app.use(express.static('public'));
 
+// handlebars
+const hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.locals.pageTitle = 'Default';
+
 // Send responses
 app.get('/', function (request, response) {
-  response.sendFile(__dirname + '/views/home.html');
+  response.render('home', { pageTitle: 'Home', activeHome: true });
 });
 
 app.get('/about', function (request, response) {
-  response.sendFile(__dirname + '/views/about.html');
+  response.render('about', { pageTitle: 'About', activeAbout: true });
 });
 
 app.get('/works', function (request, response) {
-  response.sendFile(__dirname + '/views/works.html');
+  response.render('works', { pageTitle: 'Works', activeWorks: true });
 });
 
 app.get('/gallery', function (request, response) {
-  response.sendFile(__dirname + '/views/gallery.html');
+  response.render('gallery', { pageTitle: 'Gallery', activeGallery: true });
 });
 
 app.listen(3000);
